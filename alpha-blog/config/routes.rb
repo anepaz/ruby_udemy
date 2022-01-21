477c3a2ae11753cb0e05943151745e4f7b0f4316
anepaz <anelisespaz@gmail.com>
2022-01-21 16:07:12 -0300

    config turbolinks :rocket:

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles #creates show, index, new, create, edit, update and destroy routes
+  resources :articles
 end
